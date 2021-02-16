29d2903c1ccf1ebeabbf61c6fd8533ac659906f2
Nikita <nikitarafpw@mail.ru>
2021-02-16 17:33:21 +0300

    npx prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
-console.log("Hello, Xexlet!")
+console.log("Hello, Xexlet!");
 
 console.log(_.capitalize("hello"));
-console.log(_.add(2,4));
-console.log(_.last(['one', 'two']));
+console.log(_.add(2, 4));
+console.log(_.last(["one", "two"]));
